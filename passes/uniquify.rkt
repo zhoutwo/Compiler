d61551cd90bbd9ba1dedb426b9ee203e4fcf93dd
FFyuan <tonyyuan127@gmail.com>
2018-01-27 23:52:49 -0500

    forgot to change uniquify

--- passes/uniquify.rkt
+++ passes/uniquify.rkt
@@ -3,20 +3,22 @@
 (provide uniquify)
 
 (define (uniquify-helper alist)
-  (lambda (e)
-    (match e
-           [(? symbol?) (car (lookup e alist))]
-           [(? integer?) e]
-           [(? boolean?) e]
-           [`(let ([,x ,e]) ,body) 
-             (let ([newsym (gensym x)])
-               `(let ([,newsym ,((uniquify-helper alist) e)]) ,((uniquify-helper (cons (list x newsym) alist)) body)))]
+  (lambda (rawExp)
+    (match rawExp
+           [`(has-type ,e ,t)
+             (match e
+                    [(? symbol?) `(has-type ,(car (lookup e alist)) ,t)]
+                    [(? integer?) rawExp]
+                    [(? boolean?) rawExp]
+                    [`(let ([,x ,e]) ,body) 
+                      (let ([newsym (gensym x)])
+                        `(has-type (let ([,newsym ,((uniquify-helper alist) e)]) ,((uniquify-helper (cons (list x newsym) alist)) body)) ,t))]
+                    [`(,op ,es ...)
+                      `(has-type (,op ,@(map (uniquify-helper alist) es)) ,t)]
+                    [`(read)
+                      `(has-type (read) ,t)])]
            [`(program ,type ,e)
              `(program ,type ,((uniquify-helper alist) e))]
-           [`(,op ,es ...)
-             `(,op ,@(map (uniquify-helper alist) es))]
-           [`(read)
-             `(read)]
            )))
 (define uniquify
   (lambda (e)
