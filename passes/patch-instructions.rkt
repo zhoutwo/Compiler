5a3f1846a9a9bb7756d53a215b8e37101eee04d0
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2018-01-28 08:53:32 -0500

    Fix patch-instructions

--- passes/patch-instructions.rkt
+++ passes/patch-instructions.rkt
@@ -4,33 +4,28 @@
 
 (define patch-instructions-stms
   (lambda (stms)
-    (if (null? stms) null
-      (match (car stms) 
-             [`(movq ,arg1 ,arg2)
-               (if (equal? arg1 arg2)
-                 (patch-instructions-stms (cdr stms))
-                 (if (and (equal? 'deref (car arg2)) (or (equal? 'deref (car arg1)) (equal? 'global-value (car arg1)))) 
-                   (cons `(movq ,arg1 (reg rax)) 
-                         (cons `(movq (reg rax) ,arg2) (patch-instructions-stms (cdr stms))))
-                   (cons (car stms) (patch-instructions-stms (cdr stms)))))]
-             [`(movzbq ,arg1 (deref ,n ,reg))
-               (cons `(movzbq ,arg1 (reg rax))
-                     (cons `(movq (reg rax) (deref ,n ,reg)) (patch-instructions-stms (cdr stms))))]
-             [`(cmpq ,arg1 ,arg2)
-               (if (or (equal? 'int (car arg2)) (and (equal? 'deref (car arg1)) (equal? 'deref (car arg2))))
-                 (cons `(movq ,arg2 (reg rax))
-                         (cons `(cmpq ,arg1 (reg rax)) (patch-instructions-stms (cdr stms))))
-                 (cons (car stms) (patch-instructions-stms (cdr stms))))]
-             [`(,op ,arg1 ,arg2)
-               (if (and (equal? 'deref (car arg1)) (equal? 'deref (car arg2)))
-                 (cons `(movq ,arg2 (reg rax))
-                         (cons `(,op ,arg1 (reg rax)) (patch-instructions-stms (cdr stms))))
-                 (cons (car stms) (patch-instructions-stms (cdr stms))))]
-             [else (cons (car stms) (patch-instructions-stms (cdr stms)))])))) 
+    (match stms
+         [`(movq (deref ,reg1 ,l1) (deref ,reg2 ,l2))
+                     `((movq (deref ,reg1 ,l1) (reg rax))
+                       (movq (reg rax) (deref ,reg2 ,l2)))]
+         [`(movq (global-value ,ptr) (deref ,reg2 ,l2))
+                     `((movq (global-value ,ptr) (reg rax))
+                       (movq (reg rax) (deref ,reg2 ,l2)))]
+         [`(addq (deref ,reg1 ,l1) (deref ,reg2 ,l2))
+                     `((movq (deref ,reg1 ,l1) (reg rax))
+                       (addq (reg rax) (deref ,reg2 ,l2)))]
+         [`(cmpq ,rest (int ,l2))
+                     `((movq (int ,l2) (reg rax))
+                       (cmpq ,rest (reg rax)))]
+         [`(cmpq ,rest (deref ,reg2 ,l2))
+                     `((movq (deref ,reg2 ,l2) (reg rax))
+                       (cmpq ,rest (reg rax)))]
+         [`(movzbq ,rest (deref ,reg2 ,l2))
+                     `((movzbq ,rest (reg rax))
+                       (movq (reg rax) (deref ,reg2 ,l2)))]
+         [else `(,stms)])))
 
 (define patch-instructions
   (lambda (p)
     (match p
-           [`(program ,maxn ,type ,stms) `(program ,maxn ,type ,(patch-instructions-stms stms))])))
-
-
+           [`(program ,maxn ,type ,stms) `(program ,maxn ,type ,(apply append (map patch-instructions-stms stms)))])))
