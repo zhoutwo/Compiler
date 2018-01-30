a90c24737139d44bd3e3702f7e535f4c05ec4940
zhoutwo <7986384+zhoutwo@users.noreply.github.com>
2018-01-30 11:46:12 -0500

    With default heap size, passes r0-3

--- test.rkt
+++ test.rkt
@@ -3,7 +3,7 @@
 (require "compiler.rkt")
 (require "passes/type-check.rkt")
 
-;(compiler-tests "compiler.rkt" type-check r3-passes "r0" (make-list 4))
-;(compiler-tests "compiler.rkt" type-check r3-passes "r1" (make-list 48))
-;(compiler-tests "compiler.rkt" type-check r3-passes "r2" (make-list 59))
+(compiler-tests "compiler.rkt" type-check r3-passes "r0" (make-list 4))
+(compiler-tests "compiler.rkt" type-check r3-passes "r1" (make-list 48))
+(compiler-tests "compiler.rkt" type-check r3-passes "r2" (make-list 59))
 (compiler-tests "compiler.rkt" type-check r3-passes "r3" (make-list 35))
